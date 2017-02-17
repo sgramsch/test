adb4d064b251d721233afc4dc1ce8cf9f682f0ae
sgramsch <sgramsch@gmail.com>
2017-02-17 19:57:55 +0100

    commit #1

---
+++ testmodule/src/de/test/Main.java
@@ -0,0 +1,11 @@
+package de.test;
+
+/**
+ * Created by stefan on 17.02.2017.
+ */
+public class Main {
+
+    public static void main(String [ ] args) {
+        System.out.println("Hello");
+    }
+}
